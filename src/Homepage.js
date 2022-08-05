1604dc58da51539567a4d53d4e1a797969551cc4
Abhishek kumar <abhishek1122338@gmail.com>
2022-08-05 21:25:45 +0530

    RUN

--- src/Homepage.js
+++ src/Homepage.js
@@ -14,7 +14,7 @@ function Homepage() {
       <div className="App">
         <div className="TopSection">
           <div className="LogoutSection">
-            {userid.length > 0 ? (
+            {userid !== null ? (
               <>
                 <button
                   className="Top-Heading-Button-Login"
